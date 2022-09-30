37d3bc63a046b1264ce7e1e20ac090603963509c
Jacky Jiang <t83714@gmail.com>
2022-09-30 19:10:26 +1000

    add more test cases

--- test/SQLSyntax.spec.ts
+++ test/SQLSyntax.spec.ts
@@ -7,9 +7,9 @@ describe("Test SQL interpolation", () => {
         const var1 = 123;
         const var2 = "abc";
         const query =
-            sqls`SELECT * FROM WHERE name = ${var2} AND id = ${var1}`.toQuery();
+            sqls`SELECT * FROM x WHERE name = ${var2} AND id = ${var1}`.toQuery();
         expect(query[0]).to.be.equal(
-            "SELECT * FROM WHERE name = $1 AND id = $2"
+            "SELECT * FROM x WHERE name = $1 AND id = $2"
         );
         expect(query[1]).to.have.ordered.members(["abc", 123]);
     });
@@ -23,10 +23,52 @@ describe("Test SQL interpolation", () => {
         const where = SQLSyntax.where(
             SQLSyntax.joinWithAnd([condition1, condition3, condition2])
         );
-        const query = sqls`SELECT * FROM ${where}`.toQuery();
+        const query = sqls`SELECT * FROM x ${where}`.toQuery();
         expect(query[0]).to.be.equal(
-            "SELECT * FROM  WHERE name = $1 AND id = $2"
+            "SELECT * FROM x  WHERE name = $1 AND id = $2"
         );
         expect(query[1]).to.have.ordered.members(["abc", 123]);
+    });
+});
+
+describe("Test IN clause", () => {
+    it("Should work with IN method", () => {
+        const q = sqls`SELECT * FROM b WHERE ${sqls`col1`.in([
+            3,
+            "abc",
+            9
+        ])}`.toQuery();
+        expect(q[0]).to.be.equal("SELECT * FROM b WHERE col1 IN ($1,$2,$3)");
+        expect(q[1]).to.have.ordered.members([3, "abc", 9]);
+    });
+
+    it("Should work with SQLSyntax.IN static method", () => {
+        const q = sqls`SELECT * FROM b WHERE ${SQLSyntax.in(sqls`col1`, [
+            3,
+            "abc",
+            9
+        ])}`.toQuery();
+        expect(q[0]).to.be.equal("SELECT * FROM b WHERE col1 IN ($1,$2,$3)");
+        expect(q[1]).to.have.ordered.members([3, "abc", 9]);
+    });
+
+    it("Should throw an error when empty value array", () => {
+        let err: Error;
+        try {
+            sqls`SELECT * FROM b WHERE ${sqls`col1`.in([])}`.toQuery();
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.be.instanceOf(Error);
+    });
+
+    it("Should throw an error when SQLSyntax instance is an empty statement", () => {
+        let err: Error;
+        try {
+            SQLSyntax.empty.in([]).toQuery();
+        } catch (e) {
+            err = e;
+        }
+        expect(err).to.be.instanceOf(Error);
     });
 });
